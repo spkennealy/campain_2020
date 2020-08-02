bf1025dbcb98eb87c2edc396607172114a0d144c
Sean Kennealy <spkennealy@gmail.com>
2020-08-02 13:01:23 -0700

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require("express");
-const app = express();
 const port = process.env.PORT || 5000;
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
