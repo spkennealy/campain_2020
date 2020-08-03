7a4d26e6fdc3d619bf29a841dd2cc2dc803038bf
Sean Kennealy <spkennealy@gmail.com>
2020-08-02 19:59:06 -0700

    User register route created

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const port = process.env.PORT || 5000;
 const express = require("express");
 const mongoose = require('mongoose');
 const app = express();
@@ -11,12 +10,14 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
 
 app.get("/", (req, res) => {
     res.send("Hello Sean!");
 });
 
-app.use("/api/users", users);
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
