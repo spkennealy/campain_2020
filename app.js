e2cf2a6864d9ea54bd470cbc53be658d0adcc3e3
Sean Kennealy <spkennealy@gmail.com>
2020-08-03 11:05:33 -0700

    Login route

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/api/users", users);
 
 app.get("/", (req, res) => {
     res.send("Hello Sean!");
 });
+
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 
-router.get("/users", (req, res) => {
+router.get("/", (req, res) => {
     res.json({ msg: "This is the users route" });
 });
 
@@ -33,6 +33,27 @@ router.post('/register', (req, res) => {
                     })
                 })
             }
+        })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({ email: 'This user does not exist' });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({ msg: 'Success' });
+                    } else {
+                        return res.status(400).json({ password: 'Incorrect password' });
+                    }
+                })
         })
 })
 
