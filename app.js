bc594e44d79f5c447098fdc6a937a52892457f42
Sean Kennealy <spkennealy@gmail.com>
2020-08-26 08:18:09 -0700

    Add all donations route to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
+const donations = require("./routes/api/donations");
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
@@ -21,6 +22,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", users);
+app.use("/api/donations", donations);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
